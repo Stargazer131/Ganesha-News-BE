4f424d9b96f7628b9157afafeedbc386a3081b68
Stargazer131 <naotomori131@gmail.com>
2024-08-14 22:16:45 +0700

    first working version of be

--- server.py
+++ server.py
@@ -3,12 +3,17 @@ from typing import Annotated
 from fastapi import FastAPI, Query, HTTPException
 from pymongo import MongoClient
 from model import Article, Category, ArticleRecommendation, ShortArticle, PyObjectId
-
+import pickle
+import joblib
 
 app = FastAPI()
 client = MongoClient("mongodb://localhost:27017")
 db = client["Ganesha_News"]
 
+with open('data/nndescent.pkl', 'rb') as file:
+    nndescent = pickle.load(file)
+
+topic_distributions = joblib.load('data/topic_distributions')
 
 
 @app.get("/articles/", response_model=list[ShortArticle])
@@ -30,16 +35,20 @@ def get_articles_by_category(
 
 
 @app.get("/article/{article_id}", response_model=ArticleRecommendation)
-def get_article_and_recommendations_by_id(article_id: PyObjectId):    
+def get_article_and_recommendations_by_id(
+    article_id: PyObjectId, 
+    limit: Annotated[int, Query(ge=5, le=20)] = 10,
+):    
     article = db['newspaper_v2'].find_one({"_id": article_id})
 
     if article is None:
         raise HTTPException(404, "Article not found")
+    
+    res_index, _ = nndescent.query([topic_distributions[article['index']]], k=limit+1)
+    filter_index = [int(num) for num in res_index[0][1:]]
 
-    total_docs = db['newspaper_v2'].count_documents({})
-    index_list = [random.randint(0, total_docs - 1) for i in range(10)]
     recommendation_list = db['newspaper_v2'].find(
-        {"index": {"$in": index_list}},
+        {"index": {"$in": filter_index}},
         {"title": 1, "description": 1, "thumbnail": 1}
     )
 
