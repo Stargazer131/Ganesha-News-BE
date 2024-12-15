31415674be5a0c55cf2ef85a3b9ab4597e15e3ab
Stargazer131 <naotomori131@gmail.com>
2024-12-15 16:28:36 +0700

    optimize search query

--- server/main.py
+++ server/main.py
@@ -17,15 +17,13 @@ def get_articles_by_category(
     limit: Annotated[int, Query(ge=10, le=40)] = 20,
     category: Category = Category.latest
 ):
-    filter = {}
+    query = {}
+    fields = {"title": 1, "description": 1, "thumbnail": 1}
+    sort_criteria = {"published_date": -1}
     if category != Category.latest:
-        filter = {"category": category}
-
-    articles = db['newspaper'].find(
-        filter,
-        {"title": 1, "description": 1, "thumbnail": 1}
-    ).sort({"published_date": -1}).skip((page - 1) * limit).limit(limit)
-
+        query = {"category": category}
+    
+    articles = db['newspaper'].find(query, fields).sort(sort_criteria).skip((page - 1) * limit).limit(limit)
     return [ShortArticle(**article) for article in articles]
 
 
@@ -35,43 +33,49 @@ def get_article_and_recommendations_by_id(
     limit: Annotated[int, Query(ge=5, le=20)] = 10,
 ):    
     article = db['newspaper'].find_one({"_id": article_id})
-
     if article is None:
         raise HTTPException(404, "Article not found")
     
     res_index = nndescent.neighbor_graph[0][article['index']]
     filter_index = [int(num) for num in res_index[1 : limit + 1]]
+    query = {"index": {"$in": filter_index}}
+    fields = {"title": 1, "description": 1, "thumbnail": 1}
 
-    recommendation_list = db['newspaper'].find(
-        {"index": {"$in": filter_index}},
-        {"title": 1, "description": 1, "thumbnail": 1}
-    )
-
+    recommendation_list = db['newspaper'].find(query, fields)
     article = Article(**article)
     recommendations = [ShortArticle(**item) for item in recommendation_list]
     return ArticleRecommendation(article=article, recommendations=recommendations)
 
 
-@app.get("/search/", response_model=list[ShortArticle])
+@app.get("/search/")
 def get_articles_by_keyword(
     keyword: str,
     limit: Annotated[int, Query(ge=1, le=50)] = 30,
+    page: Annotated[int, Query(ge=1, le=100)] = 1,
 ):
-    title_articles = list(db['newspaper'].find(
-        {"title": {"$regex": keyword, "$options": "i"}},
-        {"title": 1, "description": 1, "thumbnail": 1}
-    ).sort({"published_date": -1}).limit(limit))
-    
+    title_query = {
+        "$or": [
+            { "title": { "$regex": f"\\s+{keyword}$", "$options": "i" } },
+            { "title": { "$regex": f"^{keyword}\\s+", "$options": "i" } },
+            { "title": { "$regex": f"\\s+{keyword}\\s+", "$options": "i" } }
+        ]
+    }
+    description_query = {
+        "title": { "$regex": f"\\s+{keyword}\\s+", "$options": "i" },
+    }
+    fields = {"title": 1, "description": 1, "thumbnail": 1}
+    sort_criteria = {"published_date": -1}
+
+    title_articles = list(db['newspaper'].find(title_query, fields).sort(sort_criteria))
     title_ids = [article["_id"] for article in title_articles]
-    remaining = limit - len(title_ids)
-    if remaining > 0:
-        description_articles = list(db['newspaper'].find(
-            {"description": {"$regex": keyword, "$options": "i"}, "_id": {"$nin": title_ids}},
-            {"title": 1, "description": 1, "thumbnail": 1}
-        ).sort({"published_date": -1}).limit(remaining))
-    else:
-        description_articles = []
+    description_query["_id"] = {"$nin": title_ids}
+    description_articles = list(db['newspaper'].find(description_query, fields).sort(sort_criteria))
 
     articles = title_articles + description_articles
-    return [ShortArticle(**article) for article in articles]
+    start_index = min(len(articles) - 1, (page - 1) * limit)
+    end_index = min(len(articles), page * limit)
 
+    return {
+        "articles": [ShortArticle(**article) for article in articles[start_index : end_index]],
+        "total": len(articles)
+    }
